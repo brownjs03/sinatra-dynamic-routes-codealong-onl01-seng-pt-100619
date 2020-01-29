bf0346845cfa7f727df96a1674aad3cf0f518479
Jeffrey Brown <brownjs03@gmail.com>
2020-01-29 02:18:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,7 +18,12 @@ class App < Sinatra::Base
     @user_name = params[:name]
     "Goodbye, #{@user_name}." 
   end 
-
+  
+  get '/multiply/:num1/:num2' do 
+    @num_1 = params[:num1].to_i 
+    @num_2 = params[:num2].to_i
+    @num_1 * @num_2 
+  end 
   
   get '/multiply/:num1/:num2' do
     @product = params[:num1].to_i * params[:num2].to_i
